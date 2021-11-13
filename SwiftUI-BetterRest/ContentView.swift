7800c607b83f2105aa60b094768f1dfdd4262e7c
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-11-13 19:29:02 +0300

    Initial Commit

---
+++ SwiftUI-BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-BetterRest
+//
+//  Created by Никита on 13.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-BetterRest/SwiftUI_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BetterRestApp.swift
+//  SwiftUI-BetterRest
+//
+//  Created by Никита on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
